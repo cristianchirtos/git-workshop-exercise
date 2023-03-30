66f78fafe487fd2e41e221428df73c530c1a2dab
Cristian Chirtos <cristian.chirtos@wolfpack-digital.com>
2023-03-30 18:50:24 +0300

    Implement basic C++ console application

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello";
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator: +, -, *, /: ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2 << '\n';
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2 << '\n';
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2 << '\n';
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2 << '\n';
+            break;
+
+        default:
+            cout << "Operator is not available. \n";
+            break;
+    }
 
     return 0;
 }
